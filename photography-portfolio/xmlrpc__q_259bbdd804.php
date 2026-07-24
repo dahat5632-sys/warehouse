6fd5ce3d53f9abcabea9d8d0bab948be173ec1b9
dahat5632-sys <dahat5632@gmail.com>
2026-07-24 21:30:57 +0800

    Add files via upload

---
+++ photography-portfolio/xmlrpc__q_259bbdd804.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>./faq-category/eligibility-audience/index.html</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="./faq-category/eligibility-audience/index.htmlxmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="./faq-category/eligibility-audience/index.htmlxmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="./faq-category/eligibility-audience/index.htmlxmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="./faq-category/eligibility-audience/index.htmlxmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="./wp-json/index.json" />
+			</apis>
+	</service>
+</rsd>
+	
